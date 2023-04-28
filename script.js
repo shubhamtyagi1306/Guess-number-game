3ec1dc9010d7bb93e2a6d913cbd886ede124d60a
Shubham Tyagi <shubhamtyagi1306@gmail.com>
2023-04-28 19:47:15 +0530

    create highscore variable to retain the highscore of the game until browser refreshes

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
+let highScore = 0;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
@@ -14,6 +16,11 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📈 Too high!";
