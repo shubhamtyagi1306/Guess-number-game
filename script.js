25f07fdcd4a7cc2126a83f6e456902aadbbfe53d
Shubham Tyagi <shubhamtyagi1306@gmail.com>
2023-04-28 17:01:53 +0530

    add condition when guess and actual number are equal, greater and less than each other

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 "use strict";
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector(".message").textContent = "⛔ No number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Number!";
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "📈 Too high!";
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "📉 Too low!";
   }
 });
